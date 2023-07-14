6b1ed89cfaaa87a00b73ee6126fe945d6b170a97
Akbarshokh <bumur@DESKTOP>
2023-07-14 09:51:49 +0300

    plugin change

--- src/app.js
+++ src/app.js
@@ -5,23 +5,23 @@ const app = express();
 app.use(express.json());
 
 // Forward authentication route
-app.post('/auth', (req, res) => {
-  // Perform your forward authentication logic hSere
+app.post('/auth', (req, res) => { 
+  // Perform your forward authentication logic here
   // Access the request body using req.body
-  const { username, password } = req.body; 
+    const { username, password } = req.body; 
 
   // Perform authentication checks or any other processing
 
-  // Return the appropriate response
-  if (username === 'Akbarshokh' && password === '12345678') {
+  // Return the appropriate response 
+  if (username === 'Akbarshokh' && password === '12345678') { 
     res.status(200).json({ message: 'Authentication successful' });
-  } else {
+  } else { 
     res.status(401).json({ message: 'Authentication failed' });
-  }
+  } 
 });
 
 // Start the server
 const port = 80;
 app.listen(port, () => {  
   console.log(`Server is running on port ${port}`);
-});
+}); 
